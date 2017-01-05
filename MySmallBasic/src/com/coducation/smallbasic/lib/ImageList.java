cf8ad57baacec741c90943e9376da9d4c75a9c1f
Admin <Admin@ECE-222-03>
2017-01-05 21:32:03 +0900

    Math.java 완료

---
+++ MySmallBasic/src/com/coducation/smallbasic/lib/ImageList.java
@@ -0,0 +1,20 @@
+package com.coducation.smallbasic.lib;
+
+import java.util.ArrayList;
+
+import com.coducation.smallbasic.InterpretException;
+import com.coducation.smallbasic.StrV;
+import com.coducation.smallbasic.Value;
+
+public class ImageList {
+
+	public static Value LoadImage(ArrayList<Value> args){
+		return null;
+	}
+	public static Value GetWidthOfImage(ArrayList<Value> args){
+		return null;
+	}
+	public static Value GetHeightOfImage(ArrayList<Value> args){
+		return null;
+	}
+}
