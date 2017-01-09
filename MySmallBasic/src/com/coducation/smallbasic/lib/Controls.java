51234b61bf813a707caadfb8476f0f0159cc386a
user <user@DESKTOP-TQ03DM6>
2017-01-09 13:45:17 +0900

    2017.01.09.(Mon) 새로 만든 라이브러리에 notify부분 추가

--- MySmallBasic/src/com/coducation/smallbasic/lib/Controls.java
+++ MySmallBasic/src/com/coducation/smallbasic/lib/Controls.java
@@ -9,71 +9,78 @@ public class Controls {
 		// caption, left, top
 		// GraphicsWindow에 버튼을 추가한 창을 반환
 	}
-	
+
 	public static Value GetButtonCaption(ArrayList<Value> args) {
 		// buttonName
 		// buttonName에 해당하는 버튼의 caption을 반환
-		
+
 		return null;
 	}
-	
+
 	public static void SetButtonCaption(ArrayList<Value> args) {
 		// buttonName, caption
 		// buttonName에 해당하는 버튼의 caption을 설정함, 반환값 없음
-		
-		
+
 	}
-	
+
 	public static void AddTextBox(ArrayList<Value> args) {
 		// left, top
 		// GraphicsWindow에 텍스트박스를 추가한 창을 반환
 	}
-	
+
 	public static void AddMultiLineTextBox(ArrayList<Value> args) {
 		// left, top
 		// GraphicsWiindow에 텍스트박스를 추가한 창을 반환
-		
+
 	}
-	
+
 	public static Value GetTextBoxText(ArrayList<Value> args) {
 		// textBoxName
 		// textBoxName에 해당하는 텍스트 박스의 내용을 반환
-		
+
 		return null;
 	}
-	
+
 	public static void SetTextBoxText(ArrayList<Value> args) {
 		// textBoxName, text
 		// textBoxName에 해당하는 텍스트 박스의 내용을 text로 설정함
-		
+
 	}
-	
+
 	public static void Remove(ArrayList<Value> args) {
 		// controlName
 		// controlName에 해당하는 것을 삭제함, 반환값 없음
-		
+
 	}
-	
+
 	public static void Move(ArrayList<Value> args) {
 		// control, x, y
 		// control을 새로운 x, y로 옮김
 	}
-	
+
 	public static void SetSize(ArrayList<Value> args) {
 		// control, width, height
 		// control의 너비와 높이를 width와 height로 설정
 	}
-	
+
 	public static void HideControl(ArrayList<Value> args) {
 		// controlName
 		// controlName에 해당하는 control을 숨김
 	}
-	
+
 	public static void ShowControl(ArrayList<Value> args) {
 		// controlName
 		// controlName에 해당하는 control을 보임
 	}
-	
-	private static Value LastClickedButton;
-	private static Value LastTypedTextBox;
+
+	public static Value LastClickedButton;
+	public static Value LastTypedTextBox;
+
+	public static void notifyFieldAssign(String fieldName) {
+
+	}
+
+	public static void notifyFieldRead(String fieldName) {
+
+	}
 }
--- MySmallBasic/src/com/coducation/smallbasic/lib/Mouse.java
+++ MySmallBasic/src/com/coducation/smallbasic/lib/Mouse.java
@@ -8,14 +8,21 @@ public class Mouse {
 	public static void HideCursor(ArrayList<Value> args) {
 		// 화면상의 커서를 숨김
 	}
-	
+
 	public static void ShowCursor(ArrayList<Value> args) {
 		// 화면상의 커서를 보여줌
 	}
-	
-	
-	private static Value MouseX;
-	private static Value MouseY;
-	private static Value IsLeftButtonDown;
-	private static Value IsRightButtonDown;
+
+	public static Value MouseX;
+	public static Value MouseY;
+	public static Value IsLeftButtonDown;
+	public static Value IsRightButtonDown;
+
+	public static void notifyFieldAssign(String fieldName) {
+
+	}
+
+	public static void notifyFieldRead(String fieldName) {
+
+	}
 }
