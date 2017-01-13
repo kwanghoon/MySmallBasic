071595c56f858a94061f4712d3d9cdbc51bb32de
Gayoung Kim <Gayoung Kim@DESKTOP-TQ03DM6>
2017-01-13 14:04:51 +0900

    2017.01.13.(Fri) Controls, Shapes의 함수를 GraphicsWindow에 추가

--- MySmallBasic/src/com/coducation/smallbasic/lib/Shapes.java
+++ MySmallBasic/src/com/coducation/smallbasic/lib/Shapes.java
@@ -38,7 +38,7 @@ public class Shapes {
 			throw new InterpretException("Unexpected # of args " + args.size());
 	}
 
-	public static void AddEllipse(ArrayList<Value> args) {
+	public static Value AddEllipse(ArrayList<Value> args) {
 		// width, height
 		// GraphicsWindow에 원을 추가한 창을 반환
 		if (args.size() == 2) {
@@ -57,11 +57,15 @@ public class Shapes {
 				height = (int) ((StrV) args.get(1)).parseDouble();
 			} else
 				throw new InterpretException("Unexpected type " + args.get(1));
+			
+			String id = GraphicsWindow.AddEllipse(width, height);
+			
+			return new StrV(id);			
 		} else
 			throw new InterpretException("Unexpected # of args " + args.size());
 	}
 
-	public static void AddTriangle(ArrayList<Value> args) {
+	public static Value AddTriangle(ArrayList<Value> args) {
 		// x1, y1, x2, y2, x3, y3
 		// GraphicsWindow에 삼각형을 추가한 창을 반환
 		if (args.size() == 6) {
@@ -76,11 +80,14 @@ public class Shapes {
 					throw new InterpretException("Unexpected type " + args.get(i));
 			}
 
+			String id = GraphicsWindow.AddTriangle(values[0], values[1], values[2], values[3], values[4], values[5]);
+			
+			return new StrV(id);
 		} else
 			throw new InterpretException("Unexpected # of args " + args.size());
 	}
 
-	public static void AddLine(ArrayList<Value> args) {
+	public static Value AddLine(ArrayList<Value> args) {
 		// x1, y1, x2, y2
 		// GraphicsWindow에 선을 추가한 창을 반환
 		if (args.size() == 4) {
@@ -94,30 +101,44 @@ public class Shapes {
 				} else
 					throw new InterpretException("Unexpected type " + args.get(i));
 			}
+			
+			String id = GraphicsWindow.AddLine(values[0], values[1], values[2], values[3]);
+			
+			return new StrV(id);
 		} else
 			throw new InterpretException("Unexpected # of args " + args.size());
 	}
 
-	public static void AddImage(ArrayList<Value> args) {
+	public static Value AddImage(ArrayList<Value> args) {
 		// imageName
 		// GraphicsWindow에 이미지를 추가한 창을 반환
 		if (args.size() == 1) {
+			String name;
 			if (args.get(0) instanceof StrV) {
-
+				name = ((StrV) args.get(0)).getValue();
 			} else
 				throw new InterpretException("Unexpected type " + args.get(0));
+			
+			String id = GraphicsWindow.AddImage(name);
+			
+			return new StrV(id);
 		} else
 			throw new InterpretException("Unexpected # of args " + args.size());
 	}
 
-	public static void AddText(ArrayList<Value> args) {
+	public static Value AddText(ArrayList<Value> args) {
 		// text
 		// GraphicsWindow에 text를 추가한 창을 반환
 		if (args.size() == 1) {
+			String text;
 			if (args.get(0) instanceof StrV || args.get(0) instanceof DoubleV) {
-
+				text = args.get(0).toString();
 			} else
 				throw new InterpretException("Unexpected type " + args.get(0));
+			
+			String id = GraphicsWindow.AddText(text);
+			
+			return new StrV(id);
 		} else
 			throw new InterpretException("Unexpected # of args " + args.size());
 	}
@@ -126,15 +147,20 @@ public class Shapes {
 		// shapeName, text
 		// shapeName에 해당하는 shape에 text를 설정함
 		if (args.size() == 2) {
+			String name;
+			String text;
 			if (args.get(0) instanceof StrV) {
-
+				name = ((StrV) args.get(0)).getValue();
 			} else
 				throw new InterpretException("Unexpected type " + args.get(0));
 
 			if (args.get(1) instanceof StrV || args.get(1) instanceof DoubleV) {
-
+				text = args.get(1).toString();
 			} else
 				throw new InterpretException("Unexpected type " + args.get(1));
+			
+			GraphicsWindow.SetText(name, text);
+			
 		} else
 			throw new InterpretException("Unexpected # of args " + args.size());
 	}
@@ -304,10 +330,13 @@ public class Shapes {
 		// shapeName
 		// shapeName의 shape를 숨김
 		if (args.size() == 1) {
+			String name;
 			if (args.get(0) instanceof StrV) {
-
+				name = ((StrV) args.get(0)).getValue();
 			} else
 				throw new InterpretException("Unexpected type " + args.get(0));
+			
+			GraphicsWindow.HideShape(name);
 		} else
 			throw new InterpretException("Unexpected # of args " + args.size());
 	}
@@ -316,10 +345,13 @@ public class Shapes {
 		// shapeName
 		// shapeName의 shape를 보여줌
 		if (args.size() == 1) {
+			String name;
 			if (args.get(0) instanceof StrV) {
-
+				name = ((StrV) args.get(0)).getValue();
 			} else
 				throw new InterpretException("Unexpected type " + args.get(0));
+			
+			GraphicsWindow.ShowShape(name);
 		} else
 			throw new InterpretException("Unexpected # of args " + args.size());
 	}
