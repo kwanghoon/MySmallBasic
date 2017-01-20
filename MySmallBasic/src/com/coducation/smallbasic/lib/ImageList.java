f69cdb15a8d29061a1ff0f4f448192742be4ac56
Ji Yong <Ji Yong@JiYong>
2017-01-20 14:17:17 +0900

    ImageList.java 및 imagelist.sb

--- MySmallBasic/src/com/coducation/smallbasic/lib/ImageList.java
+++ MySmallBasic/src/com/coducation/smallbasic/lib/ImageList.java
@@ -1,12 +1,22 @@
 package com.coducation.smallbasic.lib;
 
+import java.awt.Image;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.ImageIcon;
 
+import com.coducation.smallbasic.DoubleV;
 import com.coducation.smallbasic.InterpretException;
 import com.coducation.smallbasic.StrV;
 import com.coducation.smallbasic.Value;
 
 public class ImageList {
+	
+	private static HashMap<String, Image> image_list = new HashMap<>();
+	
+	private static int key = 1 ;
 
 	public static Value LoadImage(ArrayList<Value> args){
 		
@@ -16,7 +26,7 @@ public class ImageList {
 			
 			if (args.get(0) instanceof StrV) {
 
-				String arg = ((StrV) args.get(0)).getValue();
+				str_arg = ((StrV) args.get(0)).getValue();
 
 			} else {
 
@@ -27,17 +37,78 @@ public class ImageList {
 		} else
 
 			throw new InterpretException("LoadImage : Unexpected # of args: " + args.size());
-
-		return null;
+		
+		if ( str_arg.contains("/") ) {
+			
+			URL url = null ;
+			
+			try {
+				url = new URL(str_arg);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+			
+			Image img = new ImageIcon(url).getImage();
+			
+			image_list.put("ImageList" + key, img);
+			
+		} else if (str_arg.contains("\\")) {
+			
+			Image img = new ImageIcon(str_arg).getImage();
+			
+			image_list.put("ImageList" + key, img);
+			
+		}
+		
+		return new StrV("ImageList" + key++);
 
 	}
 	
 	public static Value GetWidthOfImage(ArrayList<Value> args){
-		return null;
+		
+		String str_arg;
+		
+		if (args.size() == 1) {
+			
+			if (args.get(0) instanceof StrV) {
+
+				str_arg = ((StrV) args.get(0)).getValue();
+
+			} else {
+
+				throw new InterpretException("GetWidthOfImage : Unexpected arg");
+
+			}
+
+		} else
+
+			throw new InterpretException("GetWidthOfImage : Unexpected # of args: " + args.size());
+		
+		return new DoubleV(image_list.get(str_arg).getWidth(null));
 	}
 	
 	public static Value GetHeightOfImage(ArrayList<Value> args){
-		return null;
+		
+		String str_arg;
+		
+		if (args.size() == 1) {
+			
+			if (args.get(0) instanceof StrV) {
+
+				str_arg = ((StrV) args.get(0)).getValue();
+
+			} else {
+
+				throw new InterpretException("GetWidthOfImage : Unexpected arg");
+
+			}
+
+		} else
+
+			throw new InterpretException("GetWidthOfImage : Unexpected # of args: " + args.size());
+		
+		return new DoubleV(image_list.get(str_arg).getHeight(null));
+		
 	}
 	
 	public static void notifyFieldAssign(String fieldName) {
