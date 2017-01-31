237475b8714078771f671741c7b5b3a460f58824
ssjx10 <sjx10@naver.com>
2017-01-31 13:51:12 +0900

    json.jar 추가, Dictionary lib 및 regressiontest 추가

--- MySmallBasic/src/com/coducation/smallbasic/lib/Dictionary.java
+++ MySmallBasic/src/com/coducation/smallbasic/lib/Dictionary.java
@@ -1,65 +1,115 @@
 package com.coducation.smallbasic.lib;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.net.ssl.HttpsURLConnection;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.xml.sax.SAXException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import com.coducation.smallbasic.DoubleV;
 import com.coducation.smallbasic.InterpretException;
 import com.coducation.smallbasic.StrV;
 import com.coducation.smallbasic.Value;
 
-public class Dictionary {
-	
+public class Dictionary { // 옥스퍼드사전 이용
+
 	private static String u;
-	private static String photourl= "";
-	private final static int max = 4001;
+	private static String app_id = "122c166d";
+	private static String app_key = "628d19b747a4fe64e48f0879ebc20c3a";
+	private static StringBuilder def = new StringBuilder(); // 토탈정의
 
 	public static Value GetDefinition(ArrayList<Value> args) {
-		// Flickr 의 현재 사진에 대한 파일 url을 가져옴
-		u = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=264406bcdc5f0c98741b219cb5726fe7&per_page=1";
+		// 해당하는 단어의 정의를 가져옴
+		u = "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/";
 
 		if (args.size() == 1) {
 
 			if(args.get(0) instanceof StrV || args.get(0) instanceof DoubleV) {
-				u += args.get(0).toString(); // tag설정
-				String page = (int)(java.lang.Math.random()*max)+"";
-				u += "&per_page=1&page="+page; // page설정
-
+				u += args.get(0).toString().toLowerCase(); // 단어설정
+				
 				try {
 
-					DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
-					DocumentBuilder parser = f.newDocumentBuilder();
-					Document xmlDoc = parser.parse(u);
+					URL url = new URL(u);
+					HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+					urlConnection.setRequestProperty("Accept","application/json");
+					urlConnection.setRequestProperty("app_id",app_id);
+					urlConnection.setRequestProperty("app_key",app_key);
 
-					Element root = xmlDoc.getDocumentElement(); // 현재 rsp
-					String farm_id, server_id, id, secret;
-					// farm-id, server-id, photo-id, secret 필요
-					Node xmlNode1 = root.getElementsByTagName("photo").item(0);
+					// read the output from the server
+					InputStreamReader reader = new InputStreamReader(urlConnection.getInputStream(), "UTF-8");
 
-					farm_id = ((Element) xmlNode1).getAttribute("farm");
-					server_id = ((Element) xmlNode1).getAttribute("server");
-					id = ((Element) xmlNode1).getAttribute("id");
-					secret = ((Element) xmlNode1).getAttribute("secret");
+					JSONParser jsonParser = new JSONParser();
+					JSONObject object = (JSONObject) jsonParser.parse(reader);
 
-					photourl = "https://farm"+farm_id+".staticflickr.com/"+server_id+"/"+id+"_"+secret+".jpg";
+					JSONArray results_array = (JSONArray) object.get("results");
+
+					JSONArray lexicalEntries_array = (JSONArray) ((JSONObject)results_array.get(0)).get("lexicalEntries");
+
+					for(int i=0; i<lexicalEntries_array.size(); i++){
+						JSONObject lexicalEntries_object = (JSONObject) lexicalEntries_array.get(i);
+
+						String word = (String) lexicalEntries_object.get("text");
+						String lexicalCategory = (String) lexicalEntries_object.get("lexicalCategory");
+
+						JSONArray pronunciations_array = (JSONArray) lexicalEntries_object.get("pronunciations");
+						String phoneticSpelling = (String) ((JSONObject)pronunciations_array.get(0)).get("phoneticSpelling");
+						def.append(word+" [ "+phoneticSpelling+" ] "+" ( "+lexicalCategory+" )"+"\n\n");
+
+						JSONArray entries_array = (JSONArray) lexicalEntries_object.get("entries");
+						JSONArray senses_array = (JSONArray) ((JSONObject)entries_array.get(0)).get("senses");
+
+						for(int j=0; j<senses_array.size(); j++) {
+							JSONObject senses_object = (JSONObject) senses_array.get(j);
+
+							JSONArray definitions_array = (JSONArray) senses_object.get("definitions");	
+							def.append((j+1)+"."+definitions_array.get(0).toString()+"\n");
+
+							JSONArray examples_array = (JSONArray) senses_object.get("examples");
+
+							if(examples_array!=null) {
+								for(int k=0; k<examples_array.size(); k++) {
+									JSONObject examples_object = (JSONObject) examples_array.get(k);
+									def.append(examples_object.get("text")+"\n");
+								}
+							}
+
+							JSONArray subsenses_array = (JSONArray) senses_object.get("subsenses"); //부정의
+
+							if(subsenses_array!=null) {
+								for(int k=0; k<subsenses_array.size(); k++) {
+									JSONObject subsenses_object = (JSONObject) subsenses_array.get(k);
+
+									JSONArray subdefinitions_array = (JSONArray) subsenses_object.get("definitions");	
+									def.append((j+1)+"-"+(k+1)+"."+subdefinitions_array.get(0).toString()+"\n");
+
+									JSONArray subexamples_array = (JSONArray) subsenses_object.get("examples");
+
+									if(subexamples_array!=null) {
+										for(int h=0; h<subexamples_array.size(); h++) {
+											JSONObject subexamples_object = (JSONObject) subexamples_array.get(h);
+											def.append(subexamples_object.get("text")+"\n");
+										}
+									}
+									def.append("\n");
+								}
+							}
+							def.append("\n");
+						}
+						def.append("\n\n");
+					}
 
 				} catch (MalformedURLException e) {
 					e.printStackTrace();
-				} catch (ParserConfigurationException e) {
-					e.printStackTrace();
-				} catch (SAXException e) {
-					e.printStackTrace();
 				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (ParseException e) {
 					e.printStackTrace();
 				} 
 
@@ -69,7 +119,7 @@ public class Dictionary {
 		}
 		else throw new InterpretException("GetDefinition: Unexpected # of args: " + args.size());
 
-		return new StrV(photourl);
+		return new StrV(def.toString());
 	}
 
 }
