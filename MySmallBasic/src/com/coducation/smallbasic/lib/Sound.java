ac38184ed568db34ed107bdc22da91df56fdcb80
user <user@DESKTOP-SREVOBE>
2017-08-02 14:26:46 +0900

    sound 수정

--- MySmallBasic/src/com/coducation/smallbasic/lib/Sound.java
+++ MySmallBasic/src/com/coducation/smallbasic/lib/Sound.java
@@ -1,12 +1,12 @@
 package com.coducation.smallbasic.lib;
-
+ 
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
-
+ 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -15,23 +15,28 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
-
+ 
 import com.coducation.smallbasic.InterpretException;
 import com.coducation.smallbasic.StrV;
 import com.coducation.smallbasic.Value;
-
-
+ 
+ 
 public class Sound {
-
+ 
 	public static Value PlayClick(ArrayList<Value> args){
 		if(args.size()==0){
-			sound("resource\\click.wav");
+			if(st != null)
+			{
+				st.interrupt();
+			}
+			st = new SoundThread("resource\\click.wav");
+			st.start();
 			return null;
 		}
 		else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 	}
-
+ 
 	public static Value PlayClickAndWait(ArrayList<Value> args){
 		if(args.size()==0){
 			sound("resource\\click.wav");
@@ -40,16 +45,22 @@ public class Sound {
 		else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 	}
-
+ 
 	public static Value PlayChime(ArrayList<Value> args){
 		if(args.size()==0){
-			sound("resource\\chime.wav");
+			if(st != null)
+			{
+				st.interrupt();
+			}
+			st = new SoundThread("resource\\chime.wav");
+			st.start();
+ 
 			return null;
 		}
 		else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 	}
-
+ 
 	public static Value PlayChimeAndWait(ArrayList<Value> args){
 		if(args.size()==0){
 			sound("resource\\chime.wav");
@@ -58,16 +69,21 @@ public class Sound {
 		else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 	}
-
+ 
 	public static Value PlayChimes(ArrayList<Value> args){
 		if(args.size()==0){
-			sound("resource\\chimes.wav");
+			if(st != null)
+			{
+				st.interrupt();
+			}
+			st = new SoundThread("resource\\chimes.wav");
+			st.start();
 			return null;
 		}
 		else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 	}
-
+ 
 	public static Value PlayChimesAndWait(ArrayList<Value> args){
 		if(args.size()==0){
 			sound("resource\\chimes.wav");
@@ -76,16 +92,21 @@ public class Sound {
 		else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 	}
-
+ 
 	public static Value PlayBellRing(ArrayList<Value> args){
 		if(args.size()==0){
-			sound("resource\\bellring.wav");
+			if(st != null)
+			{
+				st.interrupt();
+			}
+			st = new SoundThread("resource\\bellring.wav");
+			st.start();
 			return null;
 		}
 		else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 	}
-
+ 
 	public static Value PlayBellRingAndWait(ArrayList<Value> args){
 		if(args.size()==0){
 			sound("resource\\bellring.wav");
@@ -94,12 +115,12 @@ public class Sound {
 		else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 	}
-
+ 
 	//악보...
 	public static Value PlayMusic(ArrayList<Value> args){
 		return null;
 	}
-
+ 
 	public static Value Play(ArrayList<Value> args) throws URISyntaxException{
 		if(args.size()==1){
 			Value arg = args.get(0);
@@ -110,7 +131,7 @@ public class Sound {
 			}
 			else 
 				throw new InterpretException("Not String Value for filePath" + arg);
-			
+ 
 			if(m.getPause() == PAUSE)
 			{
 				m.Start();
@@ -125,7 +146,7 @@ public class Sound {
 		else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 	}
-
+ 
 	public static Value PlayAndWait(ArrayList<Value> args){
 		if(args.size()==1){
 			Value arg = args.get(0);
@@ -136,7 +157,7 @@ public class Sound {
 			}
 			else 
 				throw new InterpretException("Not String Value for filePath" + arg);
-			
+ 
 			m = new MP3(s);
 			m.Start();
 			return null;
@@ -144,7 +165,7 @@ public class Sound {
 		else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 	}
-
+ 
 	public static Value Pause(ArrayList<Value> args){
 		if(args.size()==1){
 			Value arg = args.get(0);
@@ -161,7 +182,7 @@ public class Sound {
 		else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 	}
-
+ 
 	public static Value Stop(ArrayList<Value> args){
 		if(args.size()==1){
 			Value arg = args.get(0);
@@ -172,14 +193,14 @@ public class Sound {
 			}
 			else 
 				throw new InterpretException("Not String Value for filePath" + arg);
-			
-				m.Stop();
-				return null;
+ 
+			m.Stop();
+			return null;
 		}
 		else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 	}
-
+ 
 	public static void sound(String audioFilePath) 
 	{
 		File audioFile = new File(audioFilePath);
@@ -190,18 +211,18 @@ public class Sound {
 			SourceDataLine audioLine = (SourceDataLine) AudioSystem.getLine(info);
 			audioLine.open(format);
 			audioLine.start();
-
+ 
 			byte[] bytesBuffer = new byte[BUFFER_SIZE];
 			int bytesRead = -1;
-
+ 
 			while ((bytesRead = audioStream.read(bytesBuffer)) != -1) {
 				audioLine.write(bytesBuffer, 0, bytesRead);
 			}
 			audioLine.drain();
 			audioLine.close();
 			audioStream.close();
-
-
+ 
+ 
 		} catch (UnsupportedAudioFileException ex) {
 			ex.printStackTrace();
 		} catch (LineUnavailableException ex) {
@@ -209,33 +230,36 @@ public class Sound {
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}       
-
+ 
 	}
 	public static void notifyFieldAssign(String fieldName) {
 		//비어두기 
 	}
-
+ 
 	public static void notifyFieldRead(String fieldName) {
 		//비어두기 
 	}
-
+ 
 	private final static int BUFFER_SIZE = 4096;
 	private static MP3 m = new MP3();
 	private final static int PAUSE = 2;
+	private static SoundThread st;
+	
+	
 }
-
-
+ 
+ 
 class MP3{
-
+ 
 	private Clip clip;
 	private File wavFile;
 	private AudioInputStream ais;
 	private boolean isPlay = false;
 	private final static int PAUSE = 2;
 	private int isPause = 1;
-
+ 
 	public MP3(){}
-	
+ 
 	public MP3(String s)
 	{
 		try {
@@ -253,11 +277,11 @@ class MP3{
 				e.printStackTrace();
 			}
 		}catch (Exception e) {
-
+ 
 			e.printStackTrace();
 		}
 	}
-	
+ 
 	public void Start()
 	{
 		if(!isPlay)
@@ -269,7 +293,7 @@ class MP3{
 			}catch(Exception e){}
 		}
 	}
-
+ 
 	public void Pause()
 	{
 		if(isPlay)
@@ -279,8 +303,8 @@ class MP3{
 			isPlay = false;
 		}
 	}
-	
-
+ 
+ 
 	public void Stop()
 	{
 		if(isPlay)
@@ -290,10 +314,59 @@ class MP3{
 			isPlay = false;
 		}
 	}
-
+ 
 	public int getPause()
 	{
 		return isPause;
 	}
-	
+ 
+}
+ 
+/*
+ * Bricks.sb 예제에서 block 제거 시 나는 소리가 재생되는 동안 게임이 진행안되는 부분 수정
+ * Play...() 메소드에서 이용 
+ */
+ 
+class SoundThread extends Thread{
+ 
+	String filename;
+	Clip clip;
+ 
+	public SoundThread(String filename)
+	{
+		this.filename = filename;
+	}
+ 
+	public void run() 
+	{
+			try 
+			{
+				File file = new File(filename);
+				AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+				clip = AudioSystem.getClip();
+				clip.open(ais);
+				clip.start();
+				Thread.sleep(clip.getMicrosecondLength()/1000); //음악재생동안 sleep   
+				clip.stop();
+			} 
+			catch (UnsupportedAudioFileException e) 
+			{
+				e.printStackTrace();
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			} 
+			catch (LineUnavailableException e) 
+			{
+				e.printStackTrace();
+			}       
+			catch (InterruptedException e) 
+			{
+				if(clip != null)
+				{
+					clip.stop();
+				}
+			}
+		}
 }
