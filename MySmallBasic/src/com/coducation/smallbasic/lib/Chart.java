491530a9fbd7796342078c040723432c5c5aa63a
ssjx10 <sjx10@naver.com>
2018-03-14 16:45:03 +0900

    Chart lib 메소드 추가 및 수정
    - Chart lib 메소드(Bar, ShowLegend, HideLegend, geX, getY, getWidth,
    getHeight) 추가 및 그에 따른 GraphicsWindow.java 수정
    - Chart lib regressiontest 파일 추가

--- MySmallBasic/src/com/coducation/smallbasic/lib/Chart.java
+++ MySmallBasic/src/com/coducation/smallbasic/lib/Chart.java
@@ -55,11 +55,67 @@ public class Chart {
 		} else
 			throw new InterpretException("Error in # of Arguments:  " + args.size());
 		
-		GraphicsWindow.AddChart(key, x_value, y_value, opt);
+		GraphicsWindow.AddLine(key, x_value, y_value, opt);
 		id = GraphicsWindow.GetChartID();
 
 		return new StrV(id);
 	}
+	
+	public static Value Bar(ArrayList<Value> args) {
+		String id;
+		String key = "";
+		String[] x_value = {"0", "1", "2"};
+		double[] y_value = {0, 1, 2};
+		String opt = "";
+		if(args.size() == 4) {
+			if(args.get(1) instanceof ArrayV) {
+				if(args.get(2) instanceof ArrayV) {
+					key = args.get(0).toString();
+					int size = ((ArrayV)args.get(1)).size();
+					x_value = new String[size];
+					y_value = new double[size];
+					opt = args.get(3).toString();
+					for(int i = 0; i< size; i++) {
+						x_value[i] = ((ArrayV)args.get(1)).get(i+"").toString();
+						y_value[i] = ((ArrayV)args.get(2)).get(i+"").getNumber();
+					}
+				} 
+			}
+		} else if(args.size() == 3) {
+			if(args.get(1) instanceof ArrayV) {
+				if(args.get(2) instanceof ArrayV) {
+					key = args.get(0).toString();
+					int size = ((ArrayV)args.get(1)).size();
+					x_value = new String[size];
+					y_value = new double[size];
+					for(int i = 0; i< size; i++) {
+						x_value[i] = ((ArrayV)args.get(1)).get(i+"").toString();
+						y_value[i] = ((ArrayV)args.get(2)).get(i+"").getNumber();
+					}
+				} 
+			}
+		} else if(args.size() == 2) {
+			if(args.get(1) instanceof ArrayV) {
+				key = args.get(0).toString();
+				int size = ((ArrayV)args.get(1)).size();
+				x_value = new String[size];
+				y_value = new double[size];
+				for(int i = 0; i< size; i++) {
+					x_value[i] = i+"";
+					y_value[i] = ((ArrayV)args.get(1)).get(i+"").getNumber();
+				}
+			}
+		} else if(args.size() == 1) {
+			key = args.get(0).toString();
+		} else
+			throw new InterpretException("Error in # of Arguments:  " + args.size());
+		
+		GraphicsWindow.AddBar(key, x_value, y_value, opt);
+		id = GraphicsWindow.GetChartID();
+
+		return new StrV(id);
+	}
+
 
 	public static Value SetData(ArrayList<Value> args) {
 		if(args.size() == 4){
@@ -70,10 +126,10 @@ public class Chart {
 							String name = args.get(0).toString();
 							String key = args.get(1).toString();
 							int size = ((ArrayV)args.get(2)).size();
-							double[] x_value = new double[size];
+							String[] x_value = new String[size];
 							double[] y_value = new double[size];
 							for(int i = 0; i< size; i++) {
-								x_value[i] = ((ArrayV)args.get(2)).get(i+"").getNumber();
+								x_value[i] = ((ArrayV)args.get(2)).get(i+"").toString();
 								y_value[i] = ((ArrayV)args.get(3)).get(i+"").getNumber();
 							}
 							GraphicsWindow.SetData(name, key, x_value, y_value);
@@ -92,21 +148,33 @@ public class Chart {
 	}
 	
 	public static Value AddData(ArrayList<Value> args) {
-		if(args.size() == 4){
+		String name = args.get(0).toString();
+		String key = args.get(1).toString();
+		String[] x_value = {"0", "1", "2"};
+		double[] y_value = {0, 1, 2};
+		String opt = "";
+		if(args.size() == 5) {
+			int size = ((ArrayV)args.get(2)).size();
+			x_value = new String[size];
+			y_value = new double[size];
+			opt = args.get(4).toString();
+			for(int i = 0; i< size; i++) {
+				x_value[i] = ((ArrayV)args.get(2)).get(i+"").toString();
+				y_value[i] = ((ArrayV)args.get(3)).get(i+"").getNumber();
+			}
+		} else if(args.size() == 4) {
 			if(args.get(0) instanceof StrV) {
 				if(args.get(1) instanceof StrV) {
 					if(args.get(2) instanceof ArrayV) {
 						if(args.get(3) instanceof ArrayV) {
-							String name = args.get(0).toString();
-							String key = args.get(1).toString();
 							int size = ((ArrayV)args.get(2)).size();
-							double[] x_value = new double[size];
-							double[] y_value = new double[size];
+							x_value = new String[size];
+							y_value = new double[size];
 							for(int i = 0; i< size; i++) {
-								x_value[i] = ((ArrayV)args.get(2)).get(i+"").getNumber();
+								x_value[i] = ((ArrayV)args.get(2)).get(i+"").toString();
 								y_value[i] = ((ArrayV)args.get(3)).get(i+"").getNumber();
 							}
-							GraphicsWindow.AddData(name, key, x_value, y_value);
+							
 						} else 
 							throw new InterpretException("Not Array Value for y_value" + args.get(3));
 					} else 
@@ -117,6 +185,8 @@ public class Chart {
 
 		} else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
+		
+		GraphicsWindow.AddData(name, key, x_value, y_value, opt);
 
 		return null;
 	}
@@ -135,7 +205,7 @@ public class Chart {
 		return null;
 	}
 	public static Value XLabel(ArrayList<Value> args) {
-		if(args.size() == 1){
+		if(args.size() == 2){
 			if(args.get(0) instanceof StrV) {
 				String name = args.get(0).toString();
 				String x_label = args.get(1).toString();
@@ -148,7 +218,7 @@ public class Chart {
 		return null;
 	}
 	public static Value YLabel(ArrayList<Value> args) {
-		if(args.size() == 1){
+		if(args.size() == 2){
 			if(args.get(0) instanceof StrV) {
 				String name = args.get(0).toString();
 				String y_label = args.get(1).toString();
@@ -161,7 +231,7 @@ public class Chart {
 		return null;
 	}
 
-	public static Value Legend(ArrayList<Value> args) {
+	public static Value ShowLegend(ArrayList<Value> args) {
 		if(args.size() == 1){
 			if(args.get(0) instanceof StrV) {
 				String name = args.get(0).toString();
@@ -174,18 +244,57 @@ public class Chart {
 		return null;
 	}
 	
+	public static Value HideLegend(ArrayList<Value> args) {
+		if(args.size() == 1){
+			if(args.get(0) instanceof StrV) {
+				String name = args.get(0).toString();
+				GraphicsWindow.RemoveLegend(name);
+			} else 
+				throw new InterpretException("Not String Value for chartName" + args.get(0));
+		} else
+			throw new InterpretException("Error in # of Arguments: " + args.size());
+
+		return null;
+	}
+	
 	public static Value SetSize(ArrayList<Value> args){
 		if(args.size() == 3){
 			if(args.get(0) instanceof StrV) {
 				String name = args.get(0).toString();
-				int x = (int)args.get(1).getNumber();
-				int y = (int)args.get(2).getNumber();
-				GraphicsWindow.SetChartSize(name,x,y);
+				int w = (int)args.get(1).getNumber();
+				int h = (int)args.get(2).getNumber();
+				GraphicsWindow.SetChartSize(name,w,h);
 			} else 
 				throw new InterpretException("Not String Value for chartName" + args.get(0));
 		}else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 		return null;
+	}
+	
+	public static Value GetWidth(ArrayList<Value> args){
+		double width;
+		if(args.size() == 1){
+			if(args.get(0) instanceof StrV) {
+				String name = args.get(0).toString();
+				width = GraphicsWindow.GetChartWidth(name);
+			} else 
+				throw new InterpretException("Not String Value for chartName" + args.get(0));
+		}else
+			throw new InterpretException("Error in # of Arguments: " + args.size());
+		return new DoubleV(width);
+	}
+	
+	public static Value GetHeight(ArrayList<Value> args){
+		double height;
+		if(args.size() == 1){
+			if(args.get(0) instanceof StrV) {
+				String name = args.get(0).toString();
+				height = GraphicsWindow.GetChartHeight(name);
+			} else 
+				throw new InterpretException("Not String Value for chartName" + args.get(0));
+		}else
+			throw new InterpretException("Error in # of Arguments: " + args.size());
+		return new DoubleV(height);
 	}
 
 	public static Value SetLocation(ArrayList<Value> args){
@@ -200,6 +309,32 @@ public class Chart {
 		}else
 			throw new InterpretException("Error in # of Arguments: " + args.size());
 		return null;
+	}
+	
+	public static Value GetX(ArrayList<Value> args){
+		double x;
+		if(args.size() == 1){
+			if(args.get(0) instanceof StrV) {
+				String name = args.get(0).toString();
+				x = GraphicsWindow.GetChartX(name);
+			} else 
+				throw new InterpretException("Not String Value for chartName" + args.get(0));
+		}else
+			throw new InterpretException("Error in # of Arguments: " + args.size());
+		return new DoubleV(x);
+	}
+	
+	public static Value GetY(ArrayList<Value> args){
+		double y;
+		if(args.size() == 1){
+			if(args.get(0) instanceof StrV) {
+				String name = args.get(0).toString();
+				y = GraphicsWindow.GetChartY(name);
+			} else 
+				throw new InterpretException("Not String Value for chartName" + args.get(0));
+		}else
+			throw new InterpretException("Error in # of Arguments: " + args.size());
+		return new DoubleV(y);
 	}
 
 }
