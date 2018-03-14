9706c58c6ee7f664cd0ee42b47309d63b2a58ac9
khChoi <lazyswap@gmail.com>
2018-03-14 15:35:37 +0900

    Changed run.bat to print version info.

--- MySmallBasic/run.bat
+++ MySmallBasic/run.bat
@@ -1,2 +1,4 @@
+@echo off
+java -cp "./lib/*;./bin/" com.coducation.smallbasic.VersionInfo
 java -cp "./lib/*;./bin/" com.coducation.smallbasic.gui.MySmallBasicGUI
 
--- MySmallBasic/src/com/coducation/smallbasic/VersionInfo.java
+++ MySmallBasic/src/com/coducation/smallbasic/VersionInfo.java
@@ -2,4 +2,9 @@ package com.coducation.smallbasic;
 
 public class VersionInfo {
 	public static final String VERSION = "1.0.0";
+	
+	public static void main(String[] args) {
+		// Print the version info
+		System.out.println("My Small Basic, Version " + VersionInfo.VERSION );
+	}
 }
