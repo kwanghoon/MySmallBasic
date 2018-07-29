7c16bf729453ada406cf0cd53c687194f5c713ce
khChoi <lazyswap@gmail.com>
2018-07-30 08:52:33 +0900

    Updated the version information.

--- MySmallBasic/src/com/coducation/smallbasic/VersionInfo.java
+++ MySmallBasic/src/com/coducation/smallbasic/VersionInfo.java
@@ -1,7 +1,7 @@
 package com.coducation.smallbasic;
 
 public class VersionInfo {
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 	
 	public static void main(String[] args) {
 		// Print the version info
