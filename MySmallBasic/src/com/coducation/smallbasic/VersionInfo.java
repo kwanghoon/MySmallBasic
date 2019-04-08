1484ca9c782ace51a8a34790b2e8bd3ee94eefbf
Kwanghoon Choi <lazyswamp@gmail.com>
2019-04-08 22:09:05 +0900

    Updated the version info

--- MySmallBasic/src/com/coducation/smallbasic/VersionInfo.java
+++ MySmallBasic/src/com/coducation/smallbasic/VersionInfo.java
@@ -1,7 +1,7 @@
 package com.coducation.smallbasic;
 
 public class VersionInfo {
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 	
 	public static void main(String[] args) {
 		// Print the version info
