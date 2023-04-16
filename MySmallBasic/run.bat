587170ae89506c60f0e489432180107201da33f1
H-sooyeon <wndqhr__@naver.com>
2023-04-16 20:03:46 +0900

    run 수정

--- MySmallBasic/run.bat
+++ MySmallBasic/run.bat
@@ -1,6 +1,5 @@
-#!/bin/bash
-
-java -cp "./lib/*:./bin/" com.coducation.smallbasic.VersionInfo
-java -cp "./lib/*:./bin/" com.coducation.smallbasic.gui.MySmallBasicGUI
+@echo off
+java -cp "./lib/*;./bin/" com.coducation.smallbasic.VersionInfo
+java -cp "./lib/*;./bin/" com.coducation.smallbasic.gui.MySmallBasicGUI
 cd sbparser
 sbparser-exe.exe
